9081167f16a0a1f1b929ec5c2527abf056f07019
Shaofeng Ding <thudsf19@gmail.com>
2022-09-26 23:58:45 +0800

    add c macro for local debug

--- printd.h
+++ printd.h
@@ -1,1 +1,20 @@
 // printing for debug
+#ifdef TRACE_F
+  #define TRACE_ENTRY printf("[trace] %s:entry\n", __func__)
+  #define TRACE_EXIT printf("[trace] %s:exit\n", __func__)
+#else
+  #define TRACE_ENTRY ((void)0)
+  #define TRACE_EXIT ((void)0)
+#endif
+
+#ifdef LOCAL_DEBUG
+  #define debug(...) printf(__VA_ARGS__)
+#else
+  #define debug(...)
+#endif
+
+#ifdef LOCAL_DEBUG
+  #define debug2(fmt, ...) (printf(fmt" line : %d \n",##__VA_ARGS__,__LINE__));
+#else
+  #define debug2(...)
+#endif
